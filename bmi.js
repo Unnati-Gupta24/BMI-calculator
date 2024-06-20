b698c9eafa99237280b86d523c2560cfedc502e3
Unnati gupta <guptaunnati031@gmail.com>
2024-06-20 14:13:40 +0530

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,14 @@
+document.querySelector(".btn").addEventListener("click",function(){
+var h = parseFloat(document.getElementById("height").value);
+var w = parseFloat(document.getElementById("weight").value);
+var bmi = w/(h*h);
+var output = document.querySelector(".out");
+if (bmi < 18.6) {output.innerHTML =
+            `Under Weight : <span>${bmi}</span>`;
+ 
+}else if (bmi >= 18.6 && bmi < 24.9) {
+            output.innerHTML = `Normal : <span>${bmi}</span>`;
+        }else{ 
+            ouput.innerHTML = `Over Weight : <span>${bmi}</span>`;
+        }
+});
